0948cd0a9190daf9f9a4fcdbc8b701cc9816db9a
CarlMatthewVerano <113345631+CarlMatthewVerano@users.noreply.github.com>
2023-12-11 16:08:45 +0800

    obj

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,13 @@
-function calculateTax(income: number, taxYear?: number): number{
-    if ((taxYear || 2022) < 2022)
-        return income * 1.2;
-    return income * 1.3;
+let employee: {
+    id: number,
+    name: string,
+    // if optional name
+    // name?: string
+    retire: (date: Date) => void
+} = {
+        id: 1,
+        name: 'Mosh',
+        retire: (date: Date) =>{
+        console.log(date)
+    }
 }
-
-calculateTax(10_000, 2022)
-
-// OR
-
-function calculatesTax(income: number, taxYear = 2022): number{
-    if (taxYear < 2022)
-        return income * 1.2;
-    return income * 1.3;
-}
-
-// If you add second argument, that replaces the original param value
-calculatesTax(10_000)

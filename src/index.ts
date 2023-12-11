ce40ed76de7ad93afad065928c18db12643720b1
CarlMatthewVerano <113345631+CarlMatthewVerano@users.noreply.github.com>
2023-12-11 16:02:54 +0800

    Funcs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-// ENUMS
-// PascalCase
-const enum Size {Small = 1, Medium, Large}
-let mySize: Size = Size.Medium
-console.log(mySize)
+function calculateTax(income: number, taxYear?: number): number{
+    if ((taxYear || 2022) < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+
+calculateTax(10_000, 2022)
+
+// OR
+
+function calculatesTax(income: number, taxYear = 2022): number{
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+
+// If you add second argument, that replaces the original param value
+calculatesTax(10_000)
